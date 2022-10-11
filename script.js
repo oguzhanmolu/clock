3b893dd5ec56f2fc70b40e99dbc2bb305b57f9f7
Oguz <aka@pop-os.localdomain>
2022-10-11 18:40:37 +0300

    finished js and added the time function

--- script.js
+++ script.js
@@ -4,14 +4,16 @@ const smallHand = document.querySelector('#small-hand');
 const bigHand = document.querySelector('#big-hand');
 const secondHand = document.querySelector('#second-hand');
 
-// Time variables
-
+// Hand rotations with each second
 const handRotation = function () {
   const now = new Date();
   const seconds = now.getSeconds();
   const minutes = now.getMinutes();
+  const hours = now.getHours();
+  console.log(hours, minutes, seconds);
 
-  //   secondHand.style.transform = `rotate(${rotationDegree}deg)`;
+  smallHand.style.transform = `rotate(${hours * 30}deg)`;
+  bigHand.style.transform = `rotate(${minutes * 6}deg)`;
+  secondHand.style.transform = `rotate(${seconds * 6}deg)`;
 };
-
 setInterval(handRotation, 1000);
