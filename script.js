9438ce15ea3b34c67165046c7bbf0008ba7956ce
Oguz <aka@pop-os.localdomain>
2022-10-11 18:49:05 +0300

    fixed the first second delay and changed hand length

--- script.js
+++ script.js
@@ -3,17 +3,16 @@
 const smallHand = document.querySelector('#small-hand');
 const bigHand = document.querySelector('#big-hand');
 const secondHand = document.querySelector('#second-hand');
-
 // Hand rotations with each second
 const handRotation = function () {
   const now = new Date();
   const seconds = now.getSeconds();
   const minutes = now.getMinutes();
   const hours = now.getHours();
-  console.log(hours, minutes, seconds);
 
   smallHand.style.transform = `rotate(${hours * 30}deg)`;
   bigHand.style.transform = `rotate(${minutes * 6}deg)`;
   secondHand.style.transform = `rotate(${seconds * 6}deg)`;
 };
+handRotation();
 setInterval(handRotation, 1000);
